4b63a3e11337d6d335b9e644e5b50ad107741560
Bube <bubevelevski1@gmail.com>
2025-01-25 11:38:31 +0100

    Spawner Update
    Bubbles float to the invisible door and are destroyed on collision

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,6 +1,11 @@
+using System;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    
+    private void OnCollisionEnter(Collision other)
+    {
+        //health--;
+        Destroy(other.gameObject);
+    }
 }
--- Assets/Scripts/NormalBubble.cs
+++ Assets/Scripts/NormalBubble.cs
@@ -45,35 +45,19 @@ public class NormalBubble : MonoBehaviour
 
     private void Babl()
     {
-        SearchForDoor();
         FlyToDoor();
         Invoke("Babl", Data.bablInterval);
-    }
-    
-    private void SearchForDoor()
-    {
-        Collider[] detectedObject = Physics.OverlapSphere(transform.position, Data.SearchRadius);
-        float closestDistance = Mathf.Infinity;
-        GameObject closestDoor = null;
-        for (int i = 0; i < detectedObject.Length; i++)
-        {
-            Door d = detectedObject[i].GetComponent<Door>();
-            if (d != null)
-            {
-                float distanceToDoor = Vector3.Distance(detectedObject[i].transform.position, transform.position);
-                if (distanceToDoor < closestDistance)
-                {
-                    closestDistance = distanceToDoor;
-                    closestDoor = d.gameObject;
-                }
-            }
-        }
     }
     
     private void FlyToDoor()
     {
         Vector3 dir = (destination.transform.position - transform.position).normalized;
         rb.AddForce(dir * Data.flyStrength);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
     }
 }
 
